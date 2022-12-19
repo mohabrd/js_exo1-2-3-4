f50f646f9c5b5f28a516dd8ca29db1b2b2a3a442
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-19 12:21:15 +0100

    exo 1 js fini

---
+++ js_exo1/public/js/main.js
@@ -0,0 +1,13 @@
+let mon_prenom = "Mohamed ";
+
+console.log(mon_prenom);
+
+let age = 19;
+
+console.log(age);
+
+let presentation = mon_prenom + age;
+
+console.log(presentation);
+
+alert(presentation);
