b8d2efea93c749b13ea5ed9e86ed20f275706b59
mohabrd <bezrhoudimohamed@gmail.com>
2022-12-19 13:53:31 +0100

    exo 3 js fini

---
+++ js_exo3/public/js/main.js
@@ -0,0 +1,14 @@
+let chiffre1 = prompt();
+let chiffre2 = prompt();
+chiffre1 = Number(chiffre1);
+chiffre2 = Number(chiffre2);
+
+let plus = chiffre1 + chiffre2;
+let moins = chiffre1 - chiffre2;
+let multiplication = chiffre1 * chiffre2;
+let division = chiffre1 / chiffre2;
+
+console.log(plus)
+console.log(moins)
+console.log(multiplication)
+console.log(division)
